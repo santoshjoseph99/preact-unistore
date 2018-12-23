81ef549e2eaa7bd257f2eb6fecc43d87d04025f4
Santosh Joseph <sjoseph@groupon.com>
2018-12-22 17:31:28 -0800

    2nd component

--- src/index.js
+++ src/index.js
@@ -4,22 +4,52 @@ const h = createElement;
 import createStore from 'unistore'
 import { Provider, connect } from 'unistore/preact'
 
-let store = createStore();
-let actions = store => ({});
+let store = createStore({ count: 0, data: {count2: 1 } });
+let actions = store => ({
+  increment(state) {
+    return { count: state.count + 1 }
+  },
+  increment2(state) {
+    return {
+      data: {
+          count2: state.data.count2 + 1,
+        }
+      }
+  },
+});
 
-const App = connect('', actions)(
-  () => {
+const App = connect('count', actions)(
+  ({ count, increment }) => {
     return (
-      div([ ])
+      div([
+        p('.count', count),
+        button('.button', { onClick: increment }, '+'),
+      ])
     );
   }
 );
+
+const App2 = connect(function(s) { return s.data; }, actions)(class MyComponent extends Component {
+  constructor() {
+    super();
+  }
+
+  render({ count2, increment2 }) {
+    return (
+      div([
+        p('.count', count2),
+        button('.button', { onClick: increment2 }, '+'),
+      ])
+    );
+  }
+});
 
 render(
   h(Provider, { store: store }, [
     div(
       [
         h(App),
+        h(App2)
       ])]),
   document.getElementById('app')
 );
